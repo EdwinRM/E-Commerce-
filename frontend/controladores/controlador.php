4ae0bbe1516a67f98782f9cb64c639514d047e7e
Edwin <edwinrmondragon@gmail.com>
2018-06-16 22:16:06 -0500

    Se agregan carpetas y archivos para empezar el maquetado

---
+++ frontend/controladores/controlador.php
@@ -0,0 +1,7 @@
+<?php
+class ControladorPlantilla{
+  public function plantilla(){
+    include "vistas/plantilla.php";
+  }
+}
+ ?>
---
+++ frontend/vistas/plantilla.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Tienda Online</title>
+  </head>
+  <body>
+    <h1>Titulo</h1>
+  </body>
+</html>
