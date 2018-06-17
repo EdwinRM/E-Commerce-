69fbb0a6b73673593e62f80eb27e17e30312c94b
Edwin <edwinrmondragon@gmail.com>
2018-06-17 00:12:15 -0500

    Actualizacion y avance en diseño

--- frontend/vistas/plantilla.php
+++ frontend/vistas/plantilla.php
@@ -1,10 +1,38 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <meta charset="utf-8">
-    <title>Tienda Online</title>
+    <meta charset="UTF-8">
+
+    	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+    	<meta name="title" content="Tienda Virtual">
+
+    	<meta name="description" content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam accusantium enim esse eos officiis sit officia">
+
+    	<meta name="keyword" content="Lorem ipsum, dolor sit amet, consectetur, adipisicing, elit, Quisquam, accusantium, enim, esse">
+
+    	<title>Tienda Virtual</title><meta name="viewport" content="">
+
+      <link rel="stylesheet" href="vistas/css/plugins/bootstrap.min.css">
+
+      	<link rel="stylesheet" href="vistas/css/plugins/font-awesome.min.css">
+
+      	<link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+
+      	<link href="https://fonts.googleapis.com/css?family=Ubuntu|Ubuntu+Condensed" rel="stylesheet">
+
+        <link rel="stylesheet" href="vistas/css/plantilla.css">
+
+        <link rel="stylesheet" href="vistas/css/cabezal.css">
+
+      	<script src="vistas/js/plugins/jquery.min.js"></script>
+
+      	<script src="vistas/js/plugins/bootstrap.min.js"></script>
+
   </head>
   <body>
-    <h1>Titulo</h1>
+    <?php
+    include "modulos/cabezal.php";
+     ?>
   </body>
 </html>
